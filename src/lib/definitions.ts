8eaabfc741dac6f332057794f884665bde6f2d34
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-08-12 01:57:25 -0300

    refactor: refactorize custom types for project

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,37 +1,65 @@
-type Currency = "ARS" | "USD";
+import { Transaction } from "@/models/transaction";
 
-type Category = "Online subscriptions"
-| "Supermarket"
-| "Transport"
-| "Online shopping"
-| "Utilities"
-| "Education"
-| "Clothing" 
-| "Health"
-| "Dining out"
-| "Rent"
-| "Home services"
-| "Shopping"
-| "Gifts"
-| "Personal care"
-| "Insurance"
-| "Investments"
-| "Taxes"
-| "Miscellaneous"
-| "Salary"
-| "Refund"
-| "Cash withdrawal"
-| "Credit card payment"
-| "Other";
+export type Currency = 'ARS' | 'USD';
 
-export type Transaction = {
-  id: string;
-  amount: number;
-  date: string;
-  type: 'income' | 'expense';
-  description: string;
-  category: Category;
-  currency: Currency;
-  createdAt: string;
-  updatedAt: string;
+export type Category = 'online subscriptions'
+  | 'supermarket'
+  | 'transport'
+  | 'online shopping'
+  | 'utilities'
+  | 'education'
+  | 'clothing'
+  | 'health'
+  | 'dining out'
+  | 'rent'
+  | 'home services'
+  | 'shopping'
+  | 'gifts'
+  | 'personal care'
+  | 'insurance'
+  | 'investments'
+  | 'taxes'
+  | 'salary'
+  | 'refund'
+  | 'cash withdrawal'
+  | 'credit card payment'
+  | 'cash transfer'
+  | 'other';
+
+export type PaymentMethod = 'cash' | 'credit card' | 'debit card' | 'bank transfer' | 'digital wallet' | 'cryptocurrency';
+
+export type Series = {
+  value: number;
+  time: string; // formato yyyy-mm-dd
 }
+
+export type NewTransaction = Omit<Transaction, 'id' | 'createdAt' | 'updatedAt'| 'formattedDate'>;
+
+export type TransactionResponse = {
+  transactions: Transaction[];
+  netBalance: number;
+  totalIncome: number;
+  totalExpenses: number;
+}
+
+export type Account = {
+  id: string;
+  name: string;
+  type: 'cash' | 'online wallet' | 'bank account' | 'credit card';
+  balance: number;
+  currency: Currency;
+  createdAt: number;
+  updatedAt: number;
+  transactionsCount: number;
+  transactionsId: string[];
+}
+
+export type NewAccount = Omit<Account, 'id' | 'createdAt' | 'updatedAt' | 'transactionsCount' | 'transactions' | 'transactionsId'>;
+
+export type CRUDResult = {
+  success: true;
+  message: string;
+} | {
+  success: false;
+  error: string;
+};
