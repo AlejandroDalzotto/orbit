6f74edf27e0c7bd4c4a32451effb2164aca3c4dc
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-08-22 01:33:31 -0300

    refactor: remove unused component

--- src/components/NavLink.tsx
+++
@@ -1,9 +0,0 @@
-import Link, { LinkProps } from 'next/link'
-
-type NavLinkProps = LinkProps & { children: React.ReactNode };
-
-export default function NavLink({ children, ...props }: NavLinkProps) {
-  return (
-    <Link {...props} className='hover:underline'>{children}</Link>
-  )
-}
