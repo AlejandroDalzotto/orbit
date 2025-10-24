45980fedba48e29e5534e21c48e72274be2629ae
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-10-24 04:47:50 -0300

    feat: extend types with PaginationResult interface and update SortOption type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,15 @@ export type AppError = {
 }
 
 export type Response<T> = Promise<[AppError, null] | [null, T]>;
+
+export interface PaginationResult<T> {
+  items: Array<T>,
+  total: number,
+  page: number,
+  limit: number,
+  totalPages: number,
+  hasNext: boolean,
+  hasPrevious: boolean,
+}
+
+export type SortOption = "latest" | "oldest" | "balance" | "income" | "expenses"
