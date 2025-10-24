2bc91aafd1bf2b11a3090822aa8eaa3069b3aa57
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-10-24 04:46:57 -0300

    refactor: remove unused state and improve component structure in transactions page

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import ButtonAddTransaction from '@/components/buttons/ButtonAddTransaction';
 import Searchbar from '@/components/Searchbar';
 import TransactionList from '@/components/TransactionList';
 import TransactionsFinancialSummary from '@/components/TransactionsFinancialSummary';
 import { motion } from 'motion/react';
-import { useState } from 'react';
+import { Suspense } from 'react';
 
 export default function Home() {
-  const [searchQuery, setSearchQuery] = useState("")
-
-  const handleSearch = (query: string) => {
-    setSearchQuery(query)
-  }
 
   return (
     <motion.div
@@ -27,9 +21,9 @@ export default function Home() {
         transition={{ delay: 0.1 }}
         className="mb-12"
       >
-        <h1 className="text-4xl font-light text-white mb-2 font-mono">transactions</h1>
-        <div className="w-16 h-px bg-neutral-800 mb-4"></div>
-        <p className="text-neutral-400 font-mono text-sm">track income and expenses</p>
+        <h1 className="mb-2 font-mono text-4xl font-light text-white">transactions</h1>
+        <div className="w-16 h-px mb-4 bg-neutral-800"></div>
+        <p className="font-mono text-sm text-neutral-400">track income and expenses</p>
       </motion.div>
 
       <TransactionsFinancialSummary />
@@ -38,16 +32,22 @@ export default function Home() {
         initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.3 }}
-        className="flex flex-col sm:flex-row gap-4 mb-8"
+        className="flex flex-col gap-4 mb-8 sm:flex-row"
       >
         <div className="flex-1">
-          <Searchbar onSearch={handleSearch} />
+          <Suspense
+            fallback={
+              <div className="w-full h-10 rounded-lg bg-neutral-800 animate-pulse" />
+            }
+          >
+            <Searchbar />
+          </Suspense>
         </div>
-        {/* filters here */}
-        <ButtonAddTransaction text="Add Transaction" />
       </motion.div>
 
-      <TransactionList searchQuery={searchQuery} />
+      <Suspense fallback={<div>loading table...</div>}>
+        <TransactionList />
+      </Suspense>
     </motion.div>
   )
 }
