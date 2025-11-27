274f374584b5a3986b89c817e4068a2d5d5f69f5
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-11-27 15:25:58 -0300

    feat: add new types to the app

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,34 @@
 export type AppError = {
-  msg: string
+  message: string;
+};
+
+export interface Entity {
+  id: string | number;
+  createdAt?: string | number;
+  updatedAt?: string | number;
 }
 
 export type Response<T> = Promise<[AppError, null] | [null, T]>;
 
 export interface PaginationResult<T> {
-  items: Array<T>,
-  total: number,
-  page: number,
-  limit: number,
-  totalPages: number,
-  hasNext: boolean,
-  hasPrevious: boolean,
+  items: Array<T>;
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+  hasNext: boolean;
+  hasPrevious: boolean;
 }
 
-export type SortOption = "latest" | "oldest" | "balance" | "income" | "expenses"
+export enum PaginationLimit {
+  SMALL = 5,
+  DEFAULT = 10,
+  LARGE = 20,
+}
+
+export type SearchProps = {
+  query?: string;
+  limit: number;
+  offset: number;
+  sortBy: SortOption;
+};
