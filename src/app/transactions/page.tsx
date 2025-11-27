b39131b987e2ba93379a9e636fc87d79d861ddaf
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-11-27 15:15:07 -0300

    feat: remove react suspense modules and add initial transactions' loading

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
-import Searchbar from '@/components/Searchbar';
-import TransactionList from '@/components/TransactionList';
-import TransactionsFinancialSummary from '@/components/TransactionsFinancialSummary';
-import { motion } from 'motion/react';
-import { Suspense } from 'react';
+import ButtonAddTransaction from "@/components/buttons/ButtonAddTransaction";
+import Searchbar from "@/components/Searchbar";
+import TransactionList from "@/components/TransactionList";
+import TransactionsFinancialSummary from "@/components/TransactionsFinancialSummary";
+import { useTransactionStore } from "@/stores/transactionStore";
+import { motion } from "motion/react";
+import { useEffect } from "react";
 
 export default function Home() {
+  const loadPersistedTransactions = useTransactionStore(
+    (state) => state.loadPersistedTransactions,
+  );
+
+  // Load transactions data on mount
+  useEffect(() => {
+    loadPersistedTransactions();
+  }, [loadPersistedTransactions]);
 
   return (
     <motion.div
@@ -21,9 +31,13 @@ export default function Home() {
         transition={{ delay: 0.1 }}
         className="mb-12"
       >
-        <h1 className="mb-2 font-mono text-4xl font-light text-white">transactions</h1>
+        <h1 className="mb-2 font-mono text-4xl font-light text-white">
+          transactions
+        </h1>
         <div className="w-16 h-px mb-4 bg-neutral-800"></div>
-        <p className="font-mono text-sm text-neutral-400">track income and expenses</p>
+        <p className="font-mono text-sm text-neutral-400">
+          track income and expenses
+        </p>
       </motion.div>
 
       <TransactionsFinancialSummary />
@@ -35,19 +49,13 @@ export default function Home() {
         className="flex flex-col gap-4 mb-8 sm:flex-row"
       >
         <div className="flex-1">
-          <Suspense
-            fallback={
-              <div className="w-full h-10 rounded-lg bg-neutral-800 animate-pulse" />
-            }
-          >
-            <Searchbar />
-          </Suspense>
+          <Searchbar />
         </div>
+
+        <ButtonAddTransaction text="Add new transaction" />
       </motion.div>
 
-      <Suspense fallback={<div>loading table...</div>}>
-        <TransactionList />
-      </Suspense>
+      <TransactionList />
     </motion.div>
-  )
+  );
 }
