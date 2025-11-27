74ff0e9b8b1f00333e5ea30b7ff9c6ff40ed1521
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-11-27 15:11:23 -0300

    refactor: remove old swr hook from main page and use store instead.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,11 @@
 
 import AccountList from "@/components/AccountList";
 import ButtonAddWallet from "@/components/buttons/ButtonAddWallet";
-import { useWalletBalance } from "@/hooks/useWalletBalance";
+import { useWalletStore } from "@/stores/walletStore";
 import { motion } from "motion/react";
 
 export default function WalletPage() {
-
-  const { isLoading, error, totalBalance } = useWalletBalance()
-
-  if (isLoading || totalBalance === null) {
-    return <div className="flex items-center justify-center h-full">Loading...</div>;
-  }
-  if(error) return <div className="flex items-center justify-center h-full">Something went wrong...</div>
+  const totalBalance = useWalletStore((state) => state.totalBalance);
 
   return (
     <motion.div
@@ -28,10 +22,15 @@ export default function WalletPage() {
           transition={{ delay: 0.1 }}
           className="pb-8 border-b border-neutral-800"
         >
-          <h1 className="mb-2 text-2xl font-light tracking-wider text-white">YOUR WALLET</h1>
+          <h1 className="mb-2 text-2xl font-light tracking-wider text-white">
+            YOUR WALLET
+          </h1>
           <div className="flex items-center justify-between">
             <p className="text-sm font-light text-neutral-500">
-              Total Balance: ${totalBalance.toLocaleString("en-US", { minimumFractionDigits: 2 })}
+              Total Balance: $
+              {totalBalance.toLocaleString("en-US", {
+                minimumFractionDigits: 2,
+              })}
             </p>
             <ButtonAddWallet text="Add account" />
           </div>
@@ -39,7 +38,6 @@ export default function WalletPage() {
       </div>
 
       <AccountList />
-
     </motion.div>
-  )
+  );
 }
