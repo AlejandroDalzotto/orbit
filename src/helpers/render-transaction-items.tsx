83709505209a53ed66658b858f55b50529e70eb6
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-11-27 15:19:48 -0300

    refactor: improve list grid to show items properly aligned.

--- src/helpers/render-transaction-items.tsx
+++ src/helpers/render-transaction-items.tsx
@@ -1,23 +1,58 @@
 import { SupermarketTransaction } from "@/models/transaction";
 
 export const renderTransactionItems = (tx: SupermarketTransaction) => {
-
-
   return (
     <div>
+      {/* Headers */}
+      <div className="grid grid-cols-5 gap-2 font-mono border-b border-neutral-800 py-2 text-sm font-bold text-neutral-400">
+        <span className="col-span-3 text-left">Item</span>
+        <span className="text-center">Quantity</span>
+        <span className="text-right">Price</span>
+      </div>
+
+      {/* Rows */}
       {tx.items.map((item, index) => (
-        <div key={index} className="flex last:border-transparent justify-between border-b not-last:border-neutral-800 py-2">
-          <span>{item.name}</span>
-          {
-            item.price ? (
-              <span>${item.price.toLocaleString('en-US', { minimumFractionDigits: 2 })}</span>
+        <div
+          key={index}
+          className="grid grid-cols-5 gap-2 last:border-transparent border-b border-neutral-800 py-2"
+        >
+          {/* ITEM (3 columns) */}
+          <span className="col-span-3 truncate text-left">
+            {item.name || (
+              <span className="text-neutral-500" title="not specified">
+                n/s
+              </span>
+            )}
+          </span>
+
+          {/* QUANTITY */}
+          <span className="text-center">
+            {item.quantity ? (
+              item.quantity
             ) : (
-              <span className="text-neutral-500">no price</span>
-            )
-          }
+              <span className="text-neutral-500" title="not specified">
+                n/s
+              </span>
+            )}
+          </span>
+
+          {/* PRICE (right aligned) */}
+          <span className="text-right">
+            {item.price ? (
+              <>
+                $
+                {item.price.toLocaleString("en-US", {
+                  minimumFractionDigits: 2,
+                })}
+              </>
+            ) : (
+              <span className="text-neutral-500" title="not specified">
+                n/s
+              </span>
+            )}
+          </span>
         </div>
       ))}
     </div>
-  )
-
-}
+  );
+};
